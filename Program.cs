863d5620594c1e40fdbbc2fbf2388aa823fe276e
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-20 12:56:14 +0530

    c#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reflection in c#!");
+
+            Type T = typeof(bool);
+            Console.WriteLine("Type of Name:\t{0}", T.Name);
+            Console.WriteLine("Full name of the type defined:\t{0}", T.FullName);
+            Console.WriteLine("Type of assembly is defined:\t{0}", T.Assembly);
+            Console.WriteLine("infomation about base type:\t{0}", T.BaseType);
+
+
+        }
+    }
+}
